97b8eb7a5ed6cd137b9d68c20bb33e5c75c9f711
Mobinul Islam Mahi <mobinulislammahi@gmail.com>
2024-11-17 00:10:22 +0600

    skip eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // Skip ESLint checks during build
+    },
+  };
+  
+  export default nextConfig;
+  
